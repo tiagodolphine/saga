74964e048ac3eada7ba3567a04a34c465228d619
ruromero <rromerom@redhat.com>
2020-09-30 08:34:57 +0200

    test lra
    Signed-off-by: ruromero <rromerom@redhat.com>

--- order-service/src/main/java/org/acme/kogito/poc/sagas/services/OrderService.java
+++ order-service/src/main/java/org/acme/kogito/poc/sagas/services/OrderService.java
@@ -1,11 +1,13 @@
 package org.acme.kogito.poc.sagas.services;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import io.narayana.lra.client.NarayanaLRAClient;
 import io.smallrye.mutiny.Uni;
 import org.acme.kogito.poc.sagas.model.orders.Order;
 import org.acme.kogito.poc.sagas.model.orders.OrderStatus;
@@ -14,42 +16,39 @@ import org.acme.kogito.poc.sagas.model.orders.OrderStatus;
 public class OrderService {
 
     @Inject
-    PaymentService paymentService;
+    NarayanaLRAClient lraClient;
 
-    private Map<UUID, Order> orders = new HashMap<>();
+    private Map<String, Order> orders = new HashMap<>();
 
-    public Uni<UUID> create(Order order) {
+    public Uni<String> create(Order order) {
         return Uni.createFrom().item(order).onItem().transform(o -> {
-            UUID id = UUID.randomUUID();
+            String lraPath = lraClient.getCurrent().getPath();
+            String id = lraPath.substring(lraPath.lastIndexOf("/") + 1);
+            o.setLraPath(lraPath);
             orders.put(id, o.setStatus(OrderStatus.PENDING));
-            paymentService.reserveCredit(o.getPayment()).onItem().transform(status -> {
-                switch (status) {
-                    case APPROVED:
-                        return OrderStatus.COMPLETED;
-                    case REJECTED:
-                        return OrderStatus.FAILED;
-                }
-                return null;
-            }).subscribe().with(
-                    orderStatus -> orders.put(id, orders.get(id).setStatus(OrderStatus.COMPLETED)),
-                    failure -> orders.put(id, orders.get(id).setStatus(OrderStatus.FAILED)));
             return id;
         });
     }
 
-    public Uni<OrderStatus> getStatus(UUID id) {
+    public Uni<OrderStatus> getStatus(String lraId) {
         return Uni.createFrom()
-                .item(id)
+                .item(lraId)
                 .onItem()
-                .transform(i -> orders.get(i))
+                .transform(id -> orders.get(id))
                 .onItem()
                 .ifNotNull()
                 .transform(order -> order.getStatus());
     }
 
-    public Uni<OrderStatus> abort(UUID id) {
-        return paymentService.abort(orders.get(id).getPayment().getToken())
-                .onItem()
-                .transform(paymentStatus -> OrderStatus.FAILED);
+    public Uni<String> cancel(String lraId) {
+        return Uni.createFrom().item(lraId).onItem().invoke(id -> orders.get(id).setStatus(OrderStatus.FAILED));
+    }
+
+    public void abort(String lraId) {
+        lraClient.cancelLRA(URI.create(orders.get(lraId).getLraPath()));
+    }
+
+    public Uni<String> complete(String lraId) {
+        return Uni.createFrom().item(lraId).onItem().invoke(id -> orders.get(id).setStatus(OrderStatus.COMPLETED));
     }
 }
